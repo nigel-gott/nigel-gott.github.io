0b55c2bbf221fb17a9bc9e3b74d3f762216cb6a5
Nigel Gott <nigel.gott@gmail.com>
2019-08-20 20:13:51 +0100

    Use the mmistake theme with canned data.

---
+++ _pages/about.md
@@ -0,0 +1,5 @@
+---
+permalink: /about/
+title: "About"
+---
+It's a blog.
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Post: Standard",
+        "excerpt":"Hello world.   ","categories": [],
+        "tags": ["Intro"],
+        "url": "https://www.nigelgott.com/welcome-to-jekyll/",
+        "teaser":null}]
